acae839de57e462b8ee78adc09adda997ba6c6cf
Elimelech Ehrlich <ee525566@gmail.com>
2026-01-07 12:52:28 +0200

    creta Note claas

---
+++ repositories/noteRepository.js
@@ -0,0 +1,21 @@
+import { notes } from "../data/notes"
+
+class Note {
+    constructor(id, ownerUsername, text) {
+        this.id = id,
+        this.ownerUsername = ownerUsername,
+        this.text = text,
+        this.createdAt = new Date()
+    }
+    add() {
+        notes.push(this)
+    }
+}
+
+function listByOwner(username) {
+    
+}
+
+function deleteById(ownerUsername, noteId) {
+
+}
